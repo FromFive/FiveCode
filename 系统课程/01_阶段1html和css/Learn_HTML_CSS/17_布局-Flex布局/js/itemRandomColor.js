8573fe1c5add3836d188b919f3ed7513776336dd
FromFive <yearnsjh@sina.cn>
2022-04-14 18:02:28 +0800

    up

--- 系统课程/01_阶段1html和css/Learn_HTML_CSS/17_布局-Flex布局/js/itemRandomColor.js
+++ 系统课程/01_阶段1html和css/Learn_HTML_CSS/17_布局-Flex布局/js/itemRandomColor.js
@@ -1,8 +1,8 @@
-function getRandomColor(){
-  return 'rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})'
+function getRandomColor() {
+  return `rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`
 }
 
 const itemEls = document.querySelectorAll(".item")
 for (const item of itemEls) {
-  item.getElementsByClassName.backgroundColor=getRandomColor()
+  item.style.backgroundColor = getRandomColor()
 }
